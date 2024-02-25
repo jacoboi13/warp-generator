ed0c7cac8edffd6d9126982f88f078491bc300cc
John Car Lo Ababa <113746955+jacoboi13@users.noreply.github.com>
2024-02-25 12:33:02 +0800

    Python: Adapt for Github workflow actions
    - Use repository secrets for the ID
    - Spam and no wait time delays

--- ndbiaw.py
+++ ndbiaw.py
@@ -24,7 +24,7 @@ print('''\ \ \__ \  \ \  __\   \ \ \-.  \  \ \  __\   \ \  __<   \ \  __ \  \/_/
 print(''''\ \_____\  \ \_____\  \ \_\\"\_\   \ \_____\  \ \_\ \_\  \ \_\ \_\    \ \_\  \ \_____\  \ \_\ \_\'''')
 print('''' \/_____/   \/_____/   \/_/ \/_/   \/_____/   \/_/ /_/   \/_/\/_/     \/_/   \/_____/   \/_/ /_/''''')
 print("")
-referrer = input("Your WARP+ Public ID: ")
+referrer = os.environ('WARP_VARIABLE_ID')
 def genString(stringLength):
 	try:
 		letters = string.ascii_letters + string.digits
@@ -72,36 +72,15 @@ while True:
 		os.system('cls' if os.name == 'nt' else 'clear')
 		animation = ["[-][■□□□□□□□□□] 10%","[\][■■□□□□□□□□] 20%", "[|][■■■□□□□□□□] 30%", "[/][■■■■□□□□□□] 40%", "[-][■■■■■□□□□□] 50%", "[\][■■■■■■□□□□] 60%", "[|][■■■■■■■□□□] 70%", "[/][■■■■■■■■□□] 80%", "[-][■■■■■■■■■□] 90%", "[✓][■■■■■■■■■■] 100%"] 
 		for i in range(len(animation)):
-			time.sleep(0.5)
+			time.sleep(0.1)
 			sys.stdout.write("\r Adding traffic:" + animation[i % len(animation)])
 			sys.stdout.flush()
 		print("")
-		print(f"\n Added traffic to ID: {referrer}") 
+		print(f"\n Added traffic to inputed ID") 
 		print("")
 		print(f" {g} GB has been added to your account.")
 		print("")
 		print(f" Total: {g} Success {b} Failed")
-		print(" [|]The process will start again in 10 seconds...", end="\r")
-		time.sleep(1)
-		print(" [/]The process will start again in 9 seconds..", end="\r")
-		time.sleep(1)
-		print(" [-]The process will start again in 8 seconds.", end="\r")
-		time.sleep(1)
-		print(" [\]The process will start again in 7 seconds..", end="\r")
-		time.sleep(1)
-		print(" [|]The process will start again in 6 seconds...", end="\r")
-		time.sleep(1)
-		print(" [/]The process will start again in 5 seconds..", end="\r")
-		time.sleep(1)
-		print(" [-]The process will start again in 4 seconds.", end="\r")
-		time.sleep(1)
-		print(" [\]The process will start again in 3 seconds..", end="\r")
-		time.sleep(1)
-		print(" [|]The process will start again in 2 seconds...", end="\r")
-		time.sleep(1)
-		print(" [/]The process will start again in 1 second..", end="\r")
-		time.sleep(1)
-		print(" [-]The process will start again in 0 second.", end="\r")
 		print( " [✓]Starting the process again", end="\r")
 	else:
 		b = b + 1
